39fd4faec8caa85d1736339e55e47e6c34bfb031
leeseunggu <leesg6107@gmail.com>
2023-05-23 21:24:14 +0900

    feat: add feign

--- src/main/java/com/sgyj/accountservice/modules/account/form/SignInForm.java
+++ src/main/java/com/sgyj/accountservice/modules/account/form/SignInForm.java
@@ -1,8 +1,6 @@
 package com.sgyj.accountservice.modules.account.form;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +11,11 @@ import lombok.Setter;
 @RequiredArgsConstructor
 public class SignInForm {
 
-    @NotNull( message = "이메일을 입력해 주세요." )
-    @Email( message = "이메일 형식이 올바르지 않습니다." )
-    @Min(value = 5)
-    @Max(value = 10)
+    @NotNull(message = "이메일을 입력해 주세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @Min(value = 6)
-    @Max(value = 20)
-    @NotNull( message = "비밀번호 입력해 주세요." )
+    @NotNull(message = "비밀번호 입력해 주세요.")
     private String password;
 
 
