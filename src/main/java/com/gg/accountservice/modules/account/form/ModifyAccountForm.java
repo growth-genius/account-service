45c312c37050506053e6fbce53b8942be2c9c894
yeji.jo <choyeji1591@gmail.com>
2023-05-30 18:55:57 +0900

    fix : 계정정보 수정입력시 nickname 필수

--- src/main/java/com/gg/accountservice/modules/account/form/ModifyAccountForm.java
+++ src/main/java/com/gg/accountservice/modules/account/form/ModifyAccountForm.java
@@ -1,6 +1,7 @@
 package com.gg.accountservice.modules.account.form;
 
 import com.gg.accountservice.modules.account.enums.TravelTheme;
+import jakarta.validation.constraints.NotNull;
 import java.util.Set;
 import lombok.Data;
 
@@ -8,7 +9,8 @@ import lombok.Data;
 public class ModifyAccountForm {
 
     private String username;
-
+    
+    @NotNull(message = "별명을 입력해 주세요.")
     private String nickname;
 
     private String profileImage;
