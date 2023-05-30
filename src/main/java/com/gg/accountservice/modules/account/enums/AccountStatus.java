4a033bb6671d04737b826ad263fed7c314bfd6d6
yeji.jo <choyeji1591@gmail.com>
2023-05-30 22:12:26 +0900

    feat : AccountStatus 이메일 전송 실패 추가

--- src/main/java/com/gg/accountservice/modules/account/enums/AccountStatus.java
+++ src/main/java/com/gg/accountservice/modules/account/enums/AccountStatus.java
@@ -3,6 +3,7 @@ package com.gg.accountservice.modules.account.enums;
 public enum AccountStatus {
 
     VERIFY_EMAIL,   // 이메일 인증 단계
+    FAIL_EMAIL, // 이메일 전송 실패
     NORMAL, // 정상
     STOP,   // 중지
     SLEEP,  // 휴면
