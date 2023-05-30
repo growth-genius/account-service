2d4cf70178759cc959e232b5a7d7edfc982d8d28
yeji.jo <choyeji1591@gmail.com>
2023-05-30 22:05:30 +0900

    fix : accountId notNull 조건 추가

--- src/main/java/com/gg/accountservice/modules/account/form/ResendAuthForm.java
+++ src/main/java/com/gg/accountservice/modules/account/form/ResendAuthForm.java
@@ -11,6 +11,7 @@ public class ResendAuthForm {
     @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
+    @NotNull(message = "계정 식별자를 입력해주세요.")
     private String accountId;
 
 }
