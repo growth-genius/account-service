f218a567cb1f0c767e6419831d21ff8a8401c310
yeji.jo <choyeji1591@gmail.com>
2023-05-31 21:48:14 +0900

    fix : 이메일 토픽 이름 통일

--- src/main/java/com/gg/accountservice/modules/account/service/kafka/KafkaEmailConsumer.java
+++ src/main/java/com/gg/accountservice/modules/account/service/kafka/KafkaEmailConsumer.java
@@ -18,7 +18,7 @@ public class KafkaEmailConsumer {
     private final AccountService accountService;
 
 
-    @KafkaListener(topics = "${kafka.email-topic.send-email-fail}")
+    @KafkaListener(topics = "${kafka.email-topic.send-email-fail-topic}")
     public void sendEmailFail(String kafkaMessage) throws JsonProcessingException {
         log.error("메일 전송 실패 : {}", kafkaMessage);
         EmailMessage emailMessage = objectMapper.readValue(kafkaMessage, EmailMessage.class);
