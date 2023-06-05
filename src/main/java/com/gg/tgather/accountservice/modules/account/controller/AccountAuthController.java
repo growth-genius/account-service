1316521672adda39b0ee9c68225a6ed4e34b5994
yeji.jo <choyeji1591@gmail.com>
2023-06-05 22:32:10 +0900

    fix : 이메일 입력형식 변경

--- src/main/java/com/gg/tgather/accountservice/modules/account/controller/AccountAuthController.java
+++ src/main/java/com/gg/tgather/accountservice/modules/account/controller/AccountAuthController.java
@@ -2,7 +2,6 @@ package com.gg.tgather.accountservice.modules.account.controller;
 
 import com.gg.tgather.accountservice.modules.account.form.AccountSaveForm;
 import com.gg.tgather.accountservice.modules.account.form.AuthCodeForm;
-import com.gg.tgather.accountservice.modules.account.form.EmailAuthForm;
 import com.gg.tgather.accountservice.modules.account.form.ResendAuthForm;
 import com.gg.tgather.accountservice.modules.account.form.SignInForm;
 import com.gg.tgather.accountservice.modules.account.service.AccountService;
@@ -19,6 +18,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * account CRUD api
+ *
+ * @author joyeji
+ * @since 2023.06.05
+ */
 @RestBaseAnnotation
 @RequiredArgsConstructor
 @RequestMapping("/account/auth")
@@ -26,35 +31,70 @@ public class AccountAuthController {
 
     private final AccountService accountService;
 
+    /**
+     * 회원가입
+     *
+     * @param accountSaveForm 회원가입 입력폼
+     * @return AccountDto 회원가입 성공 계정 정보
+     */
     @PostMapping("/sign-up")
     public ApiResult<AccountDto> addUser(@RequestBody @Valid AccountSaveForm accountSaveForm) {
         return ApiUtil.success(accountService.saveAccount(accountSaveForm));
     }
 
-
+    /**
+     * 인증코드 확인
+     *
+     * @param authCodeForm 인증코드 입력폼
+     * @return AccountDto 인증코드 인증 성공 정보
+     */
     @PostMapping("/confirm-authcode")
     public ApiResult<AccountDto> validEmailByAuthCode(@RequestBody @Valid AuthCodeForm authCodeForm) {
         return ApiUtil.success(accountService.validAuthCode(authCodeForm));
     }
 
+    /**
+     * 로그인
+     *
+     * @param signInForm 로그인폼
+     * @return AccountDto 로그인 계정 정보
+     */
     @PostMapping("/sign-in")
     public ApiResult<AccountDto> login(@RequestBody @Valid SignInForm signInForm) {
         return ApiUtil.success(accountService.login(signInForm.getEmail(), new CredentialInfo(signInForm.getPassword())));
     }
 
+    /**
+     * 이메일 인증코드 재전송
+     *
+     * @param authCodeForm 이메일인증 폼
+     * @return Boolean 이메일인증코드 재전송 결과값
+     */
     @PostMapping("/resend/authcode")
     public ApiResult<Boolean> resendAuthCode(@RequestBody @Valid ResendAuthForm authCodeForm) {
         return ApiUtil.success(accountService.resendAuthCode(authCodeForm));
     }
 
+    /**
+     * 닉네임 인증
+     *
+     * @param nickname 닉네임명
+     * @return Boolean 닉네임 유효성 결과값
+     */
     @GetMapping("/check-nickname/{nickname}")
     public ApiResult<Boolean> authNickname(@PathVariable String nickname) {
         return ApiUtil.success(accountService.validNickname(nickname));
     }
 
-    @GetMapping("/check-email")
-    public ApiResult<Boolean> validEmailAddress(@RequestBody @Valid EmailAuthForm emailAuthForm) {
-        return ApiUtil.success(accountService.validEmailAddress(emailAuthForm));
+    /**
+     * 이메일 인증
+     *
+     * @param email 이메일 입력폼
+     * @return Boolean 이메일 유효성 결과값
+     */
+    @GetMapping("/check-email/{email}")
+    public ApiResult<Boolean> validEmailAddress(@PathVariable String email) {
+        return ApiUtil.success(accountService.validEmailAddress(email));
     }
 
 }
